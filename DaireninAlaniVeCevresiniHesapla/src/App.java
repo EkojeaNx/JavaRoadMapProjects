524a42fb921d08b227d0acc5b2e53cce352fa745
Türkeş Ekrem Şentürk <turkesekremsenturk@outlook.com>
2023-05-29 00:22:17 +0300

    DaireninAlaniVeCevresiniHesapla projesi eklendi

---
+++ DaireninAlaniVeCevresiniHesapla/src/App.java
@@ -0,0 +1,34 @@
+// Kullanıcıdan veri alma kütüphanesi
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Kullanıcıdan veri almak için scanner sınıfını kullanıyoruz
+        Scanner degerAl = new Scanner(System.in);
+
+        // Değişkenler
+        int r;
+        double pi = 3.14, alan, cevre, daireDilimAlan, merkezAciOlcusu;
+
+        // Kullanıcıdan dairenin yarı çapı ve merkez açı ölçüsü bilgisi alınıyor
+        System.out.print("Dairenin yarıçapını giriniz: ");
+        r = degerAl.nextInt();
+
+        System.out.print("Merkez Açısının Ölçüsü: ");
+        merkezAciOlcusu = degerAl.nextInt();
+
+        // Dairenin alanı, çevresi ve dilim alanı hesaplanıyor ve ekrana yazdırılıyor
+        alan = pi * r * r;
+        cevre = 2 * pi * r;
+
+        daireDilimAlan = (pi * (r * r) * merkezAciOlcusu) / 360;
+
+        System.out.println("Dairenin Alanı: " + alan);
+        System.out.println("Dairenin Çevresi: " + cevre);
+        System.out.println("Dairenin Merkez Açı Ölçüsü: " + merkezAciOlcusu);
+        System.out.println("Dairenin Dilim Alanı: " + daireDilimAlan);
+
+        // Scanner sınıfını kapatıyoruz
+        degerAl.close();
+    }
+}
